f030c19f9b6b04409d090c6775243480b124fbdd
Nicolas Noriega <40644310+NickN18@users.noreply.github.com>
2019-02-14 12:14:25 -0700

    Update stepper_motor.ino

--- stepper_motor.ino
+++ stepper_motor.ino
@@ -1,20 +1,50 @@
 #include <Stepper.h>
+#include <AccelStepper.h>
 
-const int stepsPerRev = 200; //change to fit the number of steps per revolution for motor
+//const int stepsPerRev = 200; //change to fit the number of steps per revolution for motor
+
+#define HALFSTEP 8
+
+#define motorPin1 7
+#define motorPin2 8
+#define motorPin3 9
+#define motorPin4 10
 
 //initializes the stepper motor library on pins 8 through 11
-Stepper testStep(stepsPerRev, 7, 8, 9, 10);
+AccelStepper testStep(HALFSTEP, motorPin1, motorPin2, motorPin3, motorPin4);
 
 int stepCount = 0;
 
 void setup() {
   // put your setup code here, to run once:
+
+  testStep.setMaxSpeed(1000.0);
+  testStep.setAcceleration(100.0);
+  testStep.setSpeed(200);
+  testStep.moveTo(4096 / 2);
+
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  testStep.step(1);
-  stepCount++;
-  delay(500);
+  if(testStep.distanceToGo() == 0) {
+    testStep.moveTo(-testStep.currentPosition());
+  }
+
+testStep.run();
+  
+/*  
+  
+  int sensorReading = analogRead(A0);
+
+  int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
+
+  if(motorSpeed > 0) {
+    testStep.setSpeed(motorSpeed);
+
+    testStep.step(stepsPerRev / 100);
+  }
+
+*/ 
 }
